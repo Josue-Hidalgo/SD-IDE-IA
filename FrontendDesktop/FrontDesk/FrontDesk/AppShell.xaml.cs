04a5fbe96e257206f0065ceb3a6224f7ec645952
Daze <dj03gamer@gmail.com>
2026-05-09 16:57:15 -0600

    FIXED
    IT COMPILES!!!

---
+++ FrontendDesktop/FrontDesk/FrontDesk/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FrontDesk
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- FrontendDesktop/Interface/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Interface")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Interface.Pages")]
