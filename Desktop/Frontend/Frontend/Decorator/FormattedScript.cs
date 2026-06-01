03dd902a21b50db7ec3f51cc8154206a9707dc38
Josue-Hidalgo <josuehidalgos2511@gmail.com>
2026-06-01 13:26:31 -0600

    Implementando Decorador - Decorador Clases Concretas

---
+++ Desktop/Frontend/Frontend/Decorator/FormattedScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.Decorator
+{
+    internal class FormattedScript : ScriptDecorator
+    {
+        private readonly string theme;
+
+        public FormattedScript(IScript inner, string theme = "dark") : base(inner)
+        {
+            this.theme = theme;
+        }
+        
+        public override string GetText() => inner.GetText();
+        public override string GetPath() => inner.GetPath();
+
+        public string GetTheme() => theme;
+    
+        /* ALGO MÁS ... */
+    }
+}
