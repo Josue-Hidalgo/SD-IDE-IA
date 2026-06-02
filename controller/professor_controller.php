b0fbc3aaba1d545282029b4008a7402988642884
Adiraell <66574277+Adiraell@users.noreply.github.com>
2026-06-01 18:01:07 -0600

    Front2web (#7) merge
    * creado el archivo de controler test.php
    
    test.php es el controler( nombre temporal). se borro de nuevo los datos en el modelo del profe.php :c
    
    * agreado el modelo del profe
    
    el archivo se borro ayer, asi que lo agrego hoy :b
    
    * agregados getters y setters
    
    mas descripcion ♎
    
    * creado la carpeta de controller y model
    
    se agrego tambien el modelo y controlador del curso
    
    * agregado controller y model de las asignaciones
    
    mas descripcion \v:/
    
    * agregado WLogin/Wregister
    
    falta terminar enviar correo y pulir el resto
    
    * actualizada la funcion de recordar contraseña
    
    * creado db_controller
    
    faltan funciones
    
    * changes on controllers
    
    cambio en prof controller y db controller
    
    * some changes to include session
    
    * some changes in courses
    
    * fix small error :b
    
    * some functions for assigments
    
    * somethings
    
    * more changes
    
    even more changes
    
    * added modify assignment function
    
    * added post
    
    * added get functions
    
    * added http codes
    
    * correction on db credential
    
    * updated post
    
    * fix a small error on login function
    
    * created student controller
    
    * added a get function
    
    * added somethings
    
    * added get enroll courses
    
    * fix a big error
    
    * Front
    
    * Update main.js
    
    * cambios
    
    * Cambios
    
    * Cambio
    
    * Cambios
    
    ---------
    
    Co-authored-by: PauloHerrera1 <132018191+PauloHerrera1@users.noreply.github.com>
    Co-authored-by: Daze <164937999+DazeEdmond@users.noreply.github.com>

---
+++ controller/professor_controller.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<?php
+include 'professor_model.php';
+
+function createProf(int $prof_id,string $email, string $password, string $username, string $userLast){
+	$_SESSION['prof_id'] = $prof_id;
+	$_SESSION['email'] = $email;
+	$_SESSION['password'] = $password;
+	$_SESSION['username'] = $username;
+	$_SESSION['userLast'] = $userLast;
+}
+
+function getProfName(Professor $prof){
+	return $_SESSION['username'];
+}
+
+function getProfEmail(Professor $prof){
+	return $_SESSION['email'];
+}
+
+function getProfLName(Professor $prof){
+	return $_SESSION['userLast'];
+}
+function getProfId(Professor $prof){
+	return $_SESSION['prof_id'];
+}
+
+?>
